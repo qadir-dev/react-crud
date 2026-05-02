d9cda43a9da0cf61e2dd770f3e2e1eef09a4d490
Qadir <qadir.qanbarpoor@gmail.com>
2026-05-01 20:55:43 -0800

    json crud

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,54 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Create from "./components/Create";
 import List from "./components/List";
 
 const App = () => {
   const [books, setBooks] = useState([]);
 
-  const handleCreate = (title) => {
-    const UpdatedBook = [...books, { id: Math.random() * 9999, title }];
+  //create
+  const handleCreate = async (title) => {
+    const res = await fetch("http://localhost:3001/books", {
+      method: "POST",
+      body: JSON.stringify({ title }),
+    });
+    const UpdatedBook = [...books, res.data];
     setBooks(UpdatedBook);
   };
 
-  const handleDelete = (id) => {
+  //read
+  const fetchBooks = async () => {
+    const res = await fetch("http://localhost:3001/books");
+    const data = await res.json();
+    setBooks(data);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  //update
+  const handleEdit = async (id, newTitle) => {
+    const res = await fetch(`http://localhost:3001/books/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({ title: newTitle }),
+    });
+    const updatedBook = books.map((book) => {
+      if (book.id === id) return { ...book, ...res.data };
+      return book;
+    });
+    setBooks(updatedBook);
+  };
+
+  const handleDelete = async (id) => {
+    const res = await fetch(`http://localhost:3001/books/${id}`, {
+      method: "DELETE",
+    });
     const updatedBook = books.filter((book) => {
       return book.id !== id;
     });
     setBooks(updatedBook);
   };
 
-  const handleEdit = (id, newTitle) => {
-    const updatedBook = books.map((book) => {
-      if (book.id === id) return { ...book, title: newTitle };
-      return book;
-    });
-    setBooks(updatedBook);
-  };
   return (
     <div className="flex flex-col flex-wrap">
       <Create onCreate={handleCreate} />
